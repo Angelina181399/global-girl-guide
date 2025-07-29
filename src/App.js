eb8fed654b207c6f7d251376f6c2b796399d4eb4
Angelina181399 <109638677+Angelina181399@users.noreply.github.com>
2025-07-29 20:58:07 +0800

    Add Vercel Speed Insights to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import OutfitPage from './components/OutfitPage';
+// src/App.js
+import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import OutfitPage from "./components/OutfitPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/outfit" element={<OutfitPage />} />
         </Routes>
+        {/* 这个组件会自动收集性能数据 */}
+        <SpeedInsights />
       </div>
     </Router>
   );
 }
 
-export default App; 
+export default App;
